2f0407fdc6253a3ddff529e79754075f85c6c356
yudinee <apdkfldbwls@icloud.com>
2025-05-30 00:48:28 +0900

    슈팅게임

--- js/Enemy.js
+++ js/Enemy.js
@@ -10,14 +10,6 @@ class Enemy extends GameObject{
         this.enemyBulletArray.push(new EnemyBullet(this.container, this ,"images/plane/ball.png",this.x, this.y, 20, 20, 0, 30 ));
     }
 
-     //내가 혹여, 주인공의 총알에 맞아서 사라질때는 그냥 가면 안됨..청소하고감(내가 쏜 여러총알들도 함께 제거..)
-    removeEnemyBullet(){
-        for(let i=0; i<this.enemyBulletArray.length; i++){
-            this.removeObject(this.enemyBulletArray, this.enemyBulletArray[i]);
-        }
-        console.log("저 가기전에 ", this.enemyBulletArray.length, "개 청소하고 가요..");
-    }
-
     tick(){
         this.x += this.velX;
         this.y += this.velY;
